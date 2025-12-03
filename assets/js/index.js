06b7e4d4746cde5c99e9fcc5cc3628698ddea090
= <=>
2025-12-04 00:10:58 +0800

    Added tech-stack section

--- assets/js/index.js
+++ assets/js/index.js
@@ -36,11 +36,34 @@ themeToggleBtn.addEventListener("click", function () {
 });
 
 /*swiper*/
- var swiper = new Swiper(".mySwiper", {
-      effect: "cards",
-      grabCursor: true,
-      autoplay:{
-        delay:1000,
-        disableOnInteraction:false,
-      }
-    });
+var swiper = new Swiper(".mySwiper", {
+  effect: "cards",
+  grabCursor: true,
+  autoplay: {
+    delay: 1000,
+    disableOnInteraction: false,
+  },
+});
+
+var swiper = new Swiper(".techSwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  loop: true,
+  autoplay: {
+    delay: 1000,
+    disableOnInteraction: false,
+  },
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
